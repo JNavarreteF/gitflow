99af1b426e0d7f26d1e6ae9509bbc84e270b06b9
Julio Navarrete <jnavarrete@floricentra.com>
2023-11-16 16:05:58 -0500

    feat(nostore): add sidebar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,50 @@
-import { AppBar, Toolbar, Button } from '@material-ui/core';
+import { AppBar, Toolbar, Button, Drawer, List, ListItem, ListItemText } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+import logo from './logo.svg';
 
 function App() {
+  const [open, setOpen] = React.useState(false);
+
+  const toggleDrawer = (open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+
+    setOpen(open);
+  };
+
+  const list = () => (
+    <div
+      role="presentation"
+      onClick={toggleDrawer(false)}
+      onKeyDown={toggleDrawer(false)}
+    >
+      <List>
+        <ListItem button key="Sales">
+          <ListItemText primary="Sales" />
+        </ListItem>
+        <ListItem button key="Purchase Orders">
+          <ListItemText primary="Purchase Orders" />
+        </ListItem>
+      </List>
+    </div>
+  );
+
   return (
     <div className="App">
       <AppBar position="static">
         <Toolbar>
+          <Button color="inherit" onClick={toggleDrawer(true)}><MenuIcon /></Button>
+          <Drawer anchor="left" open={open} onClose={toggleDrawer(false)}>
+            {list()}
+          </Drawer>
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>My App Name</h2>
           <Button color="inherit">Home</Button>
           <Button color="inherit">About</Button>
         </Toolbar>
       </AppBar>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           <code>Welcome</code> to my application.
         </p>
