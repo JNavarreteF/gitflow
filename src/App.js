407c5523951fc01c7d339239d77d35363684be62
Julio Navarrete <jnavarrete@floricentra.com>
2023-11-16 16:28:01 -0500

    feat(nostore): change nuevo

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Aprende de React
+          Aprende de React cambios exstentes
         </a>
       </header>
       <footer className="App-footer">
